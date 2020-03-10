bad10a15ab66203e96fe70aa267e68d56852d713
Nathan Matt <nmatt1@nait.ca>
2020-03-10 11:05:34 -0600

    Entity setup

--- FSISSystem/ENTITIES/Guardian.cs
+++ FSISSystem/ENTITIES/Guardian.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace FSISSystem.ENTITIES
 {
-    class Guardian
+    [Table("Guardian")]
+    public class Guardian
     {
+        public int GuardianID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmergencyPhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
     }
 }
--- FSISSystem/ENTITIES/Team.cs
+++ FSISSystem/ENTITIES/Team.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace FSISSystem.ENTITIES
 {
-    class Team
+    [Table("Team")]
+    public class Team
     {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Coach { get; set; }
+        public string AssistantCoach { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+
     }
 }
