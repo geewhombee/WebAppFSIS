5fe7aee185418661f6594dfe3b795f3b2e4abdab
Nathan Matt <nmatt1@nait.ca>
2020-03-12 15:50:00 -0600

    Part 3

--- WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
+++ WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
@@ -1,17 +1,64 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using FSISSystem.BLL;
+using FSISSystem.ENTITIES;
+
+
 namespace WebAppFSIS.ExercisePages
+
 {
     public partial class SimpleQuery : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            MessageLabel.Text = "";
+        }
 
+        protected void Fetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegionIDArg.Text))
+            {
+                MessageLabel.Text = "Enter a Team id value.";
+            }
+            else
+            {
+                int regionid = 0;
+                if (int.TryParse(RegionIDArg.Text, out regionid))
+                {
+                    if (regionid > 0)
+                    {
+                        TeamController sysmgr = new TeamController();
+                        Team info = null;
+                        info = sysmgr.Teams_FindByID(regionid); //BLL controller method
+                        if (info == null)
+                        {
+                            MessageLabel.Text = "Team ID not found.";
+                            RegionID.Text = "";
+                            RegionDescription.Text = "";
+                        }
+                        else
+                        {
+                            RegionID.Text = info.TeamID.ToString();
+                            RegionDescription.Text = info.TeamName;
+                        }
+                    }
+                    else
+                    {
+                        MessageLabel.Text = "Team id must be greater than 0";
+                    }
+
+                }
+                else
+                {
+                    MessageLabel.Text = "Team id must be a number.";
+                }
+            }
         }
     }
 }
